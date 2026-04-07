736b20fbd58808ac9dca7f92d6d9682ce48588a1
Andrew Olivier <andrew@djoinc.com>
2026-04-07 09:47:12 -0500

    Got old MCParticle demonstrations for FORM working with phlex v0.2.

--- mcparticles_module.cpp
+++ mcparticles_module.cpp
@@ -9,6 +9,6 @@ PHLEX_REGISTER_ALGORITHMS(m, config)
   auto const layer = "event"; //hard-coded in ART reader source for now
 
   m.transform("print_mcparticles", examples::print_mcparticles)
-    .input_family("largeant"_in(layer))
-    .output_products("mcparticles");
+    .input_family(product_query{.creator = "largeant", .layer = layer})
+    .output_product_suffixes("mcparticles");
 }
--- track_cheater_module.cpp
+++ track_cheater_module.cpp
@@ -9,6 +9,6 @@ PHLEX_REGISTER_ALGORITHMS(m, config)
 {
   auto cheater = m.make<examples::track_cheater>(config.get<std::map<int, double>>("energy_thresholds", {{}}));
   cheater.transform("track_cheater", &examples::track_cheater::reconstruct, concurrency::unlimited)
-    .input_family("largeant"_in("event"))
-    .output_products("cheater_tracks");
+    .input_family(product_query{.creator = "largeant", .layer = "event"})
+    .output_product_suffixes("cheater_tracks");
 }
