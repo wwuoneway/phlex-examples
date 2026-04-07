75004bd4d2496c5e88526c15c962904fdcc050e7
Andrew Olivier <andrew@djoinc.com>
2026-04-07 13:38:45 -0500

    Added some more LArSoft geometry classes to ROOT I/O dictionary.

--- dict.h
+++ dict.h
@@ -3,4 +3,5 @@
 #include "track.hpp"
 #include "gaus_hit_finder/copied_from_larsoft_minor_edits/Wire.h"
 #include "gaus_hit_finder/copied_from_larsoft_minor_edits/Hit.h"
+#include "gaus_hit_finder/copied_from_larsoft_minor_edits/geo_types.h"
 #include <vector>
