68aeb56989a2e56fafbcbeb7beda728189881afc
Andrew Olivier <andrew@djoinc.com>
2026-06-25 11:10:49 -0500

    Skeleton of how to register a user type, simb::MCParticle, for reading with FORM.  Does not yet compile.

---
+++ form_source_test_types.cpp
@@ -0,0 +1,28 @@
+#include "form/form_source_type_registry.hpp"
+#include "phlex/module.hpp"
+#include "MCParticle.h"
+
+using namespace phlex;
+
+namespace {
+  void register_extra_form_types_once()
+  {
+    static bool registered = false;
+    if (registered) {
+      return;
+    }
+
+    // Example extension point: these types are not in the built-in registry.
+    // Users can add their custom types here as needed.
+    form::experimental::register_form_vector_product_type<simb::MCParticle>("std::vector<simb::MCParticle>");
+
+    registered = true;
+  }
+}
+
+PHLEX_REGISTER_ALGORITHMS(m, config)
+{
+  (void)m;
+  (void)config;
+  register_extra_form_types_once();
+}
